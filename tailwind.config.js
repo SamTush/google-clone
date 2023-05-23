cf031b604f606ca06fa50868b427ea7ac1442b53
Samson Tush <samo4421@icloud.com>
2023-05-23 12:23:25 +0300

    Finish the layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/*.{html,js}'],
+  plugins: [],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'google-gray': '#f2f2f2',
+      'words-purple': '#271baf',
+      'words-midnight': '#4e5257',
+      'line': '#e6e7e9',
+      'outline': '#dcdcdd',
+      'user-green': '#34a853',
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+    },
   },
-  plugins: [],
 }
-
